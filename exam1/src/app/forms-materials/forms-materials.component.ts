6d59ba987de0d42bc5db537692350556d5629440
emacr <manuelcarranzaw@gmail.com>
2023-10-29 18:27:57 -0600

    cambio nuevo
    agregando angmaterial

--- exam1/src/app/forms-materials/forms-materials.component.ts
+++ exam1/src/app/forms-materials/forms-materials.component.ts
@@ -2,13 +2,15 @@ import { Component,ElementRef, ViewChild } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {  FormsModule } from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 
 @Component({
   selector: 'app-forms-materials',
   templateUrl: './forms-materials.component.html',
   styleUrls: ['./forms-materials.component.css'],
   standalone: true,
-  imports: [FormsModule, MatInputModule,MatFormFieldModule],
+  imports: [FormsModule, MatInputModule,MatFormFieldModule,MatProgressBarModule],
 })
 
 export class FormsMaterialsComponent {
